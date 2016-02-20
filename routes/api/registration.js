016e959a1d385aa74b1f7e7506275b63940b0d10
Rafael Kato <rafael@kiddolabs.com>
2016-02-20 12:41:40 -0200

    Implementing user update

--- routes/api/registration.js
+++ routes/api/registration.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const express = require('express');
+const passport = require('passport');
 const controller = require('../../controllers/api/registration');
 const router = express.Router();
 
 router.post('/donator', controller.createDonator);
+router.put('/donator/:id', passport.authenticate('jwt-api'), controller.updateDonator);
 
 router.post('/organization', controller.createOrganization);
+router.put('/organization/:id', passport.authenticate('jwt-api'), controller.updateOrganization);
 
 module.exports = router;
