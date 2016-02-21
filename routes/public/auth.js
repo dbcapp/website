46b22ce99231bc87d64ee9c2d4b4241c05ba4e10
Araan Branco <araan10@hotmail.com>
2016-02-21 14:22:29 -0300

    Stop continium loguin

--- routes/public/auth.js
+++ routes/public/auth.js
@@ -2,30 +2,31 @@
 
 const express = require("express");
 const router = express.Router();
+const User = require('../../models/user');
 
 
 router.get('/login', (req, res) => {
   res.render('login', {
-    classBody: "page"
+    classBody: "page",
+    response: req.flash()
   });
 });
 
 router.post('/login', (req, res) => {
   let params = req.body;
 
-
   if(params.email == "" || params.password == ""){
     req.flash('error', 'You email or password is incorrect!');
-    req.flash('user', params);
     res.redirect('/auth/login');
     res.end();
   } else {
-    
+    // aqui rafa
   }
 });
 
 router.get('/logout', (req, res) => {
-  res.render('logout');
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
