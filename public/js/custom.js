621a6c4c719ca02be8d545ff447fac1793ccf655
Araan Branco <araan10@hotmail.com>
2016-02-21 21:24:53 -0300

    Finish homepage

--- public/js/custom.js
+++ public/js/custom.js
@@ -20,4 +20,30 @@ $(function() {
       ['fontsize', ['fontsize']]
     ]
   });
+
+  if($('.count-commits').length > 0) {
+    $.ajax({
+      url: 'https://api.github.com/repos/dbcapp/website',
+      type: 'GET'
+    })
+    .done(function(data) {
+      console.log()
+      $('.counter-stars').html(data.stargazers_count);
+      $('.counter-forks').html(data.watchers_count);
+    });
+
+    $.ajax({
+      url: 'https://api.github.com/repos/dbcapp/website/stats/contributors',
+      type: 'GET'
+    })
+    .done(function(data) {
+      var total = 0;
+
+      data.forEach(function(item) {
+        total += item.total;
+      });
+
+      $('.count-commits').html(total);
+    });
+  }
 });
